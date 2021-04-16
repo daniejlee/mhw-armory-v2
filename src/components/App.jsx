0635dc641a28d0b51f71d905868acf1b8f7282dc
Daniel Lee <daniejlee@gmail.com>
2021-04-16 12:44:13 -0700

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    let armorType = 'chest';
+    fetch(`https://mhw-db.com/armor?q={"type":"${armorType}"}`)
+      .then(res => {
+        console.log(res)
+       return res.json()
+      })
+      .then(data => {
+        console.log(data)
+      })
+
+
+    fetch('https://mhw-db.com/armor')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
